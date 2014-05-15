4a3f07efd7ced0c3913a0cb157f354a32f91e503
Brandon C. Kelly <bckelly80@gmail.com>
2014-05-15 14:13:57 -0700

    started run_sampler file

---
+++ allstate-markov-model/allstate-markov-model/run_sampler.cpp
@@ -0,0 +1,23 @@
+//
+//  run_sampler.c
+//  allstate-markov-model
+//
+//  Created by Brandon Kelly on 5/15/14.
+//  Copyright (c) 2014 Brandon Kelly. All rights reserved.
+//
+
+#include <iostream>
+
+// local includes
+#include <samplers.hpp>
+#include <steps.hpp>
+#include "cluster.hpp"
+#include "categorical.hpp"
+#include "bounded_counts.hpp"
+#include "unbounded_counts.hpp"
+
+void run_sampler(std::vector<std::vector<int> > categorical_predictors, std::vector<std::vector<int> > bounded_counts,
+                 std::vector<std::vector<int> > unbounded_counts, unsigned int nclusters, unsigned int niter, unsigned int nburn)
+{
+    
+}

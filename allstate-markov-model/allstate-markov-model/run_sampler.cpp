7af813c530c4b1a141b26b370274c3739dd371d0
Brandon C. Kelly <bckelly80@gmail.com>
2014-05-16 10:55:00 -0700

    function declarations for MCMC sampler

--- allstate-markov-model/allstate-markov-model/run_sampler.cpp
+++ allstate-markov-model/allstate-markov-model/run_sampler.cpp
@@ -6,25 +6,5 @@
 //  Copyright (c) 2014 Brandon Kelly. All rights reserved.
 //
 
-#include <iostream>
-
 // local includes
-#include <samplers.hpp>
-#include <steps.hpp>
-#include "cluster.hpp"
-#include "categorical.hpp"
-#include "bounded_counts.hpp"
-#include "unbounded_counts.hpp"
-#include "markov_chain.hpp"
-
-Sampler build_sampler(std::vector<std::vector<int> > categorical_predictors, std::vector<std::vector<int> > bounded_counts,
-                      std::vector<std::vector<int> > unbounded_counts, unsigned int nclusters)
-{
-    
-}
-
-void run_sampler(std::vector<std::vector<int> > categorical_predictors, std::vector<std::vector<int> > bounded_counts,
-                 std::vector<std::vector<int> > unbounded_counts, unsigned int nclusters, unsigned int niter, unsigned int nburn)
-{
-    
-}
+#include "run_sampler.hpp"
